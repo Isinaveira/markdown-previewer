b9106bf53ba7a56115f049889d07439d88be8feb
isinaveira <isinaveira@gmail.com>
2025-01-20 18:47:05 +0100

    project structure & dependencies generated

---
+++ markdown-previewer/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Badge from 'react-bootstrap/Badge';
+export default class App extends React.Component{
+  render(){
+    return (
+      <div className ='App'>
+        <div className="container">
+          
+        </div>
+      </div>
+    )
+  }
+}
